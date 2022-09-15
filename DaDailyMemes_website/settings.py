89f1f65966be58a3fa72fe01fc1fe46782fb66c6
Julian Ogata <ogatajulian@gmail.com>
2022-09-15 08:56:58 -0700

    Created templates for Website

--- DaDailyMemes_website/settings.py
+++ DaDailyMemes_website/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PROJECT_DIR = os.path.join(BASE_DIR, "DaDailyMemes_website")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'DaDailyMemes_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PROJECT_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- DaDailyMemes_website/views.py
+++ DaDailyMemes_website/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Homepage")
+    return render(request, 'index.html')
+    # template = loader.get_template('index.html')
+    # return HttpResponse(template.render({}, request))
