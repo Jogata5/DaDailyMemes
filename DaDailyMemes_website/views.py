da9c4de239dc28fb09665d606b63b0e321002334
Julian Ogata <ogatajulian@gmail.com>
2022-09-15 11:39:54 -0700

    Updated Files, Need Fixing

--- DaDailyMemes_website/views.py
+++ DaDailyMemes_website/views.py
@@ -1,8 +1,17 @@
-from django.http import HttpResponse
-from django.template import loader
+# from django.http import HttpResponse
+# from django.template import loader
 from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
     # template = loader.get_template('index.html')
     # return HttpResponse(template.render({}, request))
+
+def about(request):
+    return render(request, 'about.html')
+
+def signin(request):
+    return render(request, 'signin.html')
+
+def contact(request):
+    return render(request, 'contact.html')
