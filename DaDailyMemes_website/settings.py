90c66cb5f628277f865566f4d22b3db3592a70a0
Julian Ogata <ogatajulian@gmail.com>
2022-09-20 15:56:48 -0700

    updates

--- DaDailyMemes_website/settings.py
+++ DaDailyMemes_website/settings.py
@@ -15,7 +15,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 PROJECT_DIR = os.path.join(BASE_DIR, "DaDailyMemes_website")
 
 # Quick-start development settings - unsuitable for production
