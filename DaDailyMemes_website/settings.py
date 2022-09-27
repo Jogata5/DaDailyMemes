78c7cf4a774f88784b31c69e6aa3ef67e84ae8b3
Niccolo <niccolochuidian@csu.fullerton.edu>
2022-09-27 14:55:22 -0700

    updated contact page (not finished yet)

--- DaDailyMemes_website/settings.py
+++ DaDailyMemes_website/settings.py
@@ -116,20 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-#STATIC_URL = '/static/'
-
-
-
 STATIC_URL = '/static/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-#MEDIA_URL = '/media/'
-
-
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static")
-    ]
-
-
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 LOGIN_URL = "login"
