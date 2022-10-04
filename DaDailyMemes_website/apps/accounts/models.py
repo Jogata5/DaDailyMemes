c6d9bc6342c3daf64334ffce5e2cfe065929709b
Julian Ogata <ogatajulian@gmail.com>
2022-10-04 11:19:49 -0700

    added class and 1 line of code to models

--- DaDailyMemes_website/apps/accounts/models.py
+++ DaDailyMemes_website/apps/accounts/models.py
@@ -1,3 +1,7 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
+
+class CreateUserProfile(models.Model):
+    is_full_name_displayed = models.BooleanField(default=True)
