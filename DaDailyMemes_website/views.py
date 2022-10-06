0e47c4cbe08b859602f1d6995cd8f6996a1f84dd
Julian Ogata <54689126+Jogata5@users.noreply.github.com>
2022-10-06 16:45:46 -0700

    Delete DaDailyMemes_website directory

--- DaDailyMemes_website/views.py
+++
@@ -1,8 +0,0 @@
-# from django.http import HttpResponse
-# from django.template import loader
-from django.views.generic.base import TemplateView 
-from django.contrib.auth.mixins import LoginRequiredMixin
-
-
-class ProfileView(LoginRequiredMixin,TemplateView):
-    template_name = 'accounts/profile.html'
