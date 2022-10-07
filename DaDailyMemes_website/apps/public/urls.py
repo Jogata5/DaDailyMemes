bdc5e3f6b883daf850a4f1c6b449194b96b8d7e4
Julian Ogata <54689126+Jogata5@users.noreply.github.com>
2022-10-06 17:12:46 -0700

    Add files via upload

---
+++ DaDailyMemes_website/apps/public/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path
+
+# from DaDailyMemes.DaDailyMemes_website.views import index
+from . import views
+
+app_name="public"
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('about', views.about, name="about"),
+    path('contact', views.contact, name="contact"),
+
+    
+]
---
+++ DaDailyMemes_website/apps/public/views.py
@@ -0,0 +1,18 @@
+# from django.http import HttpResponse
+# from django.template import loader
+from django.shortcuts import render
+from django.views.generic.base import TemplateView 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+class ProfileView(LoginRequiredMixin,TemplateView):
+    template_name = 'accounts/profile.html'
