d59f0073c8d7d7365c4abe9145080238c11386fa
Julian Ogata <ogatajulian@gmail.com>
2022-12-06 17:17:50 -0800

    updating settings for postgres

--- dadailymemes_website/settings.py
+++ dadailymemes_website/settings.py
@@ -74,6 +74,10 @@ WSGI_APPLICATION = 'dadailymemes_website.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+os.environ['POSTGRES_DB'] = 'dadailymemes-database'
+os.environ['POSTGRES_USER'] = 'dadailymeme-user'
+os.environ['POSTGRES_PASSWORD'] = 'postgres'
+os.environ['POSTGRES_HOST'] = '34.83.243.168'
 
 POSTGRES_DB = os.environ.get("POSTGRES_DB", default='')
 POSTGRES_USER = os.environ.get("POSTGRES_USER", default='')
