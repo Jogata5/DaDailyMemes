047dce7cc7f5f242c368d7b6bfb1bd63fce0a8a9
Julian Ogata <ogatajulian@gmail.com>
2022-12-07 19:40:58 -0800

    add registers in admin.py

--- dadailymemes_website/apps/accounts/admin.py
+++ dadailymemes_website/apps/accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, EmailJob, Genres
 
 # Register your models here.
 
 admin.site.register(UserProfile)
+admin.site.register(EmailJob)
+admin.site.register(Genres)
