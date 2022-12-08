a6aa2811d23973041448613f810ab62f9eb01791
Julian Ogata <ogatajulian@gmail.com>
2022-12-08 00:30:04 -0800

    first new models

--- dadailymemes_website/apps/jobs/jobs.py
+++ dadailymemes_website/apps/jobs/jobs.py
@@ -1,65 +1,65 @@
-# import smtplib
+import smtplib
 
-# from django.core.mail import send_mail
-# from accounts.models import EmailJob, User, Genres
-# from public.giphy import get_gif
-# from datetime import time
+from django.core.mail import send_mail
+from accounts.models import EmailJob, User, Genres
+from public.giphy import get_gif
+from datetime import time
 
 
-# def scheduleTimer():
-#     users = User.objects.all()
-#     for user in users:
-#         if hasEmailJob(user) is False:
-#             genres = []
-#             job = EmailJob()
-#             job.user = user
-#             job.job_time = time
+def scheduleTimer():
+    users = User.objects.all()
+    for user in users:
+        if hasEmailJob(user) is False:
+            genres = []
+            job = EmailJob()
+            job.user = user
+            job.job_time = time
             
-#             for genre in Genres.objects.filter(user):
-#                 if genre is True:
-#                     genres.append(genre)
-#             job.gif = get_gif(genres)
-#         else:
-#             EmailUser(user)
+            for genre in Genres.objects.filter(user):
+                if genre is True:
+                    genres.append(genre)
+            job.gif = get_gif(genres)
+        else:
+            EmailUser(user)
 
-# def EmailUser(user_arg):
-#     if checkTimer(user_arg):
-#         sendEmail(user_arg)
+def EmailUser(user_arg):
+    if checkTimer(user_arg):
+        sendEmail(user_arg)
 
-# def sendEmail(user_arg):
-#     user_email = user_arg.objects.get('email')
-#     sender = 'jogta@csu.fullerton.edu'
-#     receivers = [user_email]
-#     gif = get_gif(user_arg.objects.get())
+def sendEmail(user_arg):
+    user_email = user_arg.objects.get('email')
+    sender = 'jogta@csu.fullerton.edu'
+    receivers = [user_email]
+    gif = get_gif(user_arg.objects.get())
 
-#     message = f"""From: From Person <{sender}>
-#     To: To Person <{receivers}>
-#     Subject: SMTP email test
+    message = f"""From: From Person <{sender}>
+    To: To Person <{receivers}>
+    Subject: SMTP email test
     
-#     {gif}
+    {gif}
 
-#     This is a test message.
-#     """
-#     try:
-#         smtpObj = smtplib.SMTP('34.83.166.149')
-#         smtpObj.sendmail(sender, receivers, message)      
-#         has_job_val = user_arg.objects.get('has_job')
-#         has_job_val.value = False
-#         print("Successfully sent email")
-#     except smtplib.SMTPException:
-#         print("Error sending email")
+    This is a test message.
+    """
+    try:
+        smtpObj = smtplib.SMTP('34.83.166.149')
+        smtpObj.sendmail(sender, receivers, message)      
+        has_job_val = user_arg.objects.get('has_job')
+        has_job_val.value = False
+        print("Successfully sent email")
+    except smtplib.SMTPException:
+        print("Error sending email")
 
-# def checkTimer(user_arg):
-#     time = EmailJob.objects.filter(user_arg).values('job time')
-#     next_time = EmailJob.objects.filter(user_arg).values('next_job')
-#     if time >= next_time:
-#         return True
-#     return False
+def checkTimer(user_arg):
+    time = EmailJob.objects.filter(user_arg).values('job time')
+    next_time = EmailJob.objects.filter(user_arg).values('next_job')
+    if time >= next_time:
+        return True
+    return False
 
-# def hasEmailJob(user_arg):
-#     if EmailJob.objects.filter(user_arg).values('has_job') is True:
-#         return True
-#     return False
+def hasEmailJob(user_arg):
+    if EmailJob.objects.filter(user_arg).values('has_job') is True:
+        return True
+    return False
         
     
         
--- dadailymemes_website/apps/jobs/updater.py
+++ dadailymemes_website/apps/jobs/updater.py
@@ -1,8 +1,8 @@
-# from datetime import datetime
-# from apscheduler.schedulers.background import BackgroundScheduler
-# from .jobs import scheduleEmail, scheduleTimer
+from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+from .jobs import scheduleEmail, scheduleTimer
 
-# def start():
-#     scheduler = BackgroundScheduler
-#     scheduler.add_job(scheduleTimer, 'interval', seconds = 5)
-#     scheduler.start()
+def start():
+    scheduler = BackgroundScheduler
+    scheduler.add_job(scheduleTimer, 'interval', seconds = 5)
+    scheduler.start()
