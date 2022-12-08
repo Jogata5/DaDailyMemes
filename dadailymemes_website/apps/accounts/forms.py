4a00e52c8101942ccc6e8044c270dac05e75a982
Julian Ogata <ogatajulian@gmail.com>
2022-12-08 00:05:18 -0800

    migrations

--- dadailymemes_website/apps/accounts/forms.py
+++ dadailymemes_website/apps/accounts/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.contrib.auth.models import User
 from django import forms 
 from .models import UserProfile
-#from .models import RegisterUser
+# from .models import RegisterUser
 
 
 MEME_CATEGORIES = (
